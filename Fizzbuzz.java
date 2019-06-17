e55b1ea02e8d10e03acbddea27bfba8778697358
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-17 13:33:32 -0500

    Add files via upload

---
+++ Fizzbuzz.java
@@ -0,0 +1,13 @@
+public class Fizzbuzz {
+    public String fizzBuzz(int num) {
+        if(num%3==0 && num%5==0) {
+            return "FizzBuzz";
+        } else if(num%3==0) {
+            return "Fizz";
+        } else if(num%5==0){
+            return "Buzz";
+        } else {
+            return ""+num;
+        }
+    }
+}
